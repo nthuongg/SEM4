6d8cb3802258373451f48d8f5bafda15d5bf1e37
Nguyen Huong <nguyenhuongg1104@gmail.com>
2024-09-06 11:29:29 +0700

    upload project

--- Flutter/crudUseApiPublic/lib/services/product_service.dart
+++ Flutter/crudUseApiPublic/lib/services/product_service.dart
@@ -3,8 +3,8 @@ import 'package:http/http.dart' as http;
 import '../models/product.dart';
 import 'package:flutter/material.dart';
 
-class ApiService {
-  final String apiUrl = 'https://t2210m-flutter.onrender.com/products';
+class ProductService {
+  final String apiUrl = 'https://run.mocky.io/v3/5462303a-547b-4971-8312-acba91939577';
 
   Future<List<Product>> fetchProducts() async {
     final response = await http.get(Uri.parse(apiUrl));
